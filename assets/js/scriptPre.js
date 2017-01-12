65acf5fb41bac3a85c978048db5538f7eef9ad6f
EmeraldLiu <1309810@c126-24.ad.ccsd.net>
2017-01-12 11:07:49 -0800

    upload site

---
+++ assets/js/scriptPre.js
@@ -0,0 +1,270 @@
+$(document).ready(function(){
+    $("#washingtonPic").click(function(){
+        $("#washington").toggle();
+    });
+
+});
+
+$(document).ready(function(){
+    $("#adamsPic").click(function(){
+        $("#adams").toggle();
+    });
+});
+
+$(document).ready(function(){
+    $("#jeffersonPic").click(function(){
+        $("#jefferson").toggle();
+    });
+});
+
+$(document).ready(function(){
+    $("#madisonPic").click(function(){
+        $("#madison").toggle();
+    });
+});
+
+$(document).ready(function(){
+    $("#monroePic").click(function(){
+        $("#monroe").toggle();
+    });
+});
+
+$(document).ready(function(){
+    $("#qadamsPic").click(function(){
+        $("#qadams").toggle();
+    });
+});
+
+$(document).ready(function(){
+    $("#jacksonPic").click(function(){
+        $("#jackson").toggle();
+    });
+});
+
+$(document).ready(function(){
+    $("#vanburenPic").click(function(){
+        $("#vanburen").toggle();
+    });
+});
+
+$(document).ready(function(){
+    $("#wharrisonPic").click(function(){
+        $("#wharrison").toggle();
+    });
+});
+
+$(document).ready(function(){
+    $("#tylerPic").click(function(){
+        $("#tyler").toggle();
+    });
+});
+
+$(document).ready(function(){
+    $("#polkPic").click(function(){
+        $("#polk").toggle();
+    });
+});
+
+$(document).ready(function(){
+    $("#taylorPic").click(function(){
+        $("#taylor").toggle();
+    });
+});
+
+$(document).ready(function(){
+    $("#fillmorePic").click(function(){
+        $("#fillmore").toggle();
+    });
+});
+
+$(document).ready(function(){
+    $("#piercePic").click(function(){
+        $("#pierce").toggle();
+    });
+});
+
+$(document).ready(function(){
+    $("#buchananPic").click(function(){
+        $("#buchanan").toggle();
+    });
+});
+
+$(document).ready(function(){
+    $("#lincolnPic").click(function(){
+        $("#lincoln").toggle();
+    });
+});
+
+$(document).ready(function(){
+    $("#ajohnsonPic").click(function(){
+        $("#ajohnson").toggle();
+    });
+});
+
+$(document).ready(function(){
+    $("#grantPic").click(function(){
+        $("#grant").toggle();
+    });
+});
+
+$(document).ready(function(){
+    $("#hayesPic").click(function(){
+        $("#hayes").toggle();
+    });
+});
+
+$(document).ready(function(){
+    $("#garfieldPic").click(function(){
+        $("#garfield").toggle();
+    });
+});
+
+$(document).ready(function(){
+    $("#arthurPic").click(function(){
+        $("#arthur").toggle();
+    });
+});
+
+$(document).ready(function(){
+    $("#clevelandPic").click(function(){
+        $("#cleveland").toggle();
+    });
+});
+
+$(document).ready(function(){
+    $("#bharrisonPic").click(function(){
+        $("#bharrison").toggle();
+    });
+});
+
+$(document).ready(function(){
+    $("#clevelandPic2").click(function(){
+        $("#cleveland2").toggle();
+    });
+});
+
+$(document).ready(function(){
+    $("#mckinleyPic").click(function(){
+        $("#mckinley").toggle();
+    });
+});
+
+$(document).ready(function(){
+    $("#trooseveltPic").click(function(){
+        $("#troosevelt").toggle();
+    });
+});
+
+$(document).ready(function(){
+    $("#taftPic").click(function(){
+        $("#taft").toggle();
+    });
+});
+
+$(document).ready(function(){
+    $("#wilsonPic").click(function(){
+        $("#wilson").toggle();
+    });
+});
+
+$(document).ready(function(){
+    $("#hardingPic").click(function(){
+        $("#harding").toggle();
+    });
+});
+
+$(document).ready(function(){
+    $("#coolidgePic").click(function(){
+        $("#coolidge").toggle();
+    });
+});
+
+$(document).ready(function(){
+    $("#hooverPic").click(function(){
+        $("#hoover").toggle();
+    });
+});
+
+$(document).ready(function(){
+    $("#frooseveltPic").click(function(){
+        $("#froosevelt").toggle();
+    });
+});
+
+$(document).ready(function(){
+    $("#trumanPic").click(function(){
+        $("#truman").toggle();
+    });
+});
+
+$(document).ready(function(){
+    $("#eisenhowerPic").click(function(){
+        $("#eisenhower").toggle();
+    });
+});
+
+$(document).ready(function(){
+    $("#kennedyPic").click(function(){
+        $("#kennedy").toggle();
+    });
+});
+
+$(document).ready(function(){
+    $("#ljohnson").click(function(){
+        $("#ljohnson").toggle();
+    });
+});
+
+$(document).ready(function(){
+    $("#nixonPic").click(function(){
+        $("#nixon").toggle();
+    });
+});
+
+$(document).ready(function(){
+    $("#fordPic").click(function(){
+        $("#ford").toggle();
+    });
+});
+
+$(document).ready(function(){
+    $("#carterPic").click(function(){
+        $("#carter").toggle();
+    });
+});
+
+$(document).ready(function(){
+    $("#reaganPic").click(function(){
+        $("#reagan").toggle();
+    });
+});
+
+$(document).ready(function(){
+    $("#bushPic").click(function(){
+        $("#bush").toggle();
+    });
+});
+
+$(document).ready(function(){
+    $("#clintonPic").click(function(){
+        $("#clinton").toggle();
+    });
+});
+
+$(document).ready(function(){
+    $("#wbushPic").click(function(){
+        $("#wbush").toggle();
+    });
+});
+
+$(document).ready(function(){
+    $("#obamaPic").click(function(){
+        $("#obama").toggle();
+    });
+});
+
+$(document).ready(function(){
+    $("#trumpPic").click(function(){
+        $("#trump").toggle();
+    });
+});
---
+++ assets/js/scriptPre2.js
@@ -0,0 +1,20 @@
+$(document).ready(function(){
+
+  $('div.presInfo').each(function() {
+    var $presInfo = $(this);
+
+    $("img.presPic", $presInfo).click(function(e) {
+      e.preventDefault();
+      $currentInfo = $("div.info-container", $presInfo);
+      $currentInfo.toggle();
+      $("div.info-container").not($currentInfo).hide();
+      return false;
+    });
+
+});
+    
+  $('html').click(function(){
+    $("div.info-container").hide();
+  });
+     
+});
