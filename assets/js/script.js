d705a7556e3002a72827a50da666cea3e67a19c1
EmeraldLiu <emeraldliu0529@gmail.com>
2017-01-21 18:02:43 -0800

    oh wait im doing the nav....

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,26 @@
+//navigation//
+$(document).ready(function() {
 
+  var $toggleButton = $('.toggle-button'),
+      $menuWrap = $('.menu-wrap'),
+      $sidebarArrow = $('.sidebar-menu-arrow');
 
+  // Hamburger button
+
+  $toggleButton.on('click', function() {
+    $(this).toggleClass('button-open');
+    $menuWrap.toggleClass('menu-show');
+  });
+
+  // Sidebar navigation arrows
+
+  $sidebarArrow.click(function() {
+    $(this).next().slideToggle(300);
+  });
+
+});
+
+// President page //
 $(document).ready(function(){
 
   $('div.presInfo').each(function() {
