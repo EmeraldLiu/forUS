4e7425077443d2e7f135c9d40b8e55aa37fa5aa9
solsnhan <solangenhan@Solanges-MBP.T-mobile.com>
2017-01-21 19:20:32 -0800

    get rid of the second fade

--- assets/js/backtotop.js
+++ assets/js/backtotop.js
@@ -1,8 +1,6 @@
 jQuery(document).ready(function($){
   // browser window scroll (in pixels) after which the "back to top" link is shown
   var offset = 500,
-    //browser window scroll (in pixels) after which the "back to top" link opacity is reduced
-    offset_opacity = 2000,
     //duration of the top scrolling animation (in ms)
     scroll_top_duration = 700,
     //grab the "back to top" link
@@ -11,9 +9,6 @@ jQuery(document).ready(function($){
   //hide or show the "back to top" link
   $(window).scroll(function(){
     ( $(this).scrollTop() > offset ) ? $back_to_top.addClass('cd-is-visible') : $back_to_top.removeClass('cd-is-visible cd-fade-out');
-    if( $(this).scrollTop() > offset_opacity ) { 
-      $back_to_top.addClass('cd-fade-out');
-    }
   });
 
   //smooth scroll to top
