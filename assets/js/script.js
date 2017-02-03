63a17f9ca553361e7f40d7fb14589cecd1956c9b
EmeraldLiu <1309810@c126-24.ad.ccsd.net>
2017-02-03 11:07:13 -0800

    combined backstop js with regular js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,25 +1,49 @@
-//navigation//
-$(document).ready(function() {
+//Navigation//
+  $(document).ready(function() {
 
-  var $toggleButton = $('.toggle-button'),
-      $menuWrap = $('.menu-wrap'),
-      $sidebarArrow = $('.sidebar-menu-arrow');
+    var $toggleButton = $('.toggle-button'),
+        $menuWrap = $('.menu-wrap'),
+        $sidebarArrow = $('.sidebar-menu-arrow');
 
-  // Hamburger button
+    // Hamburger button
 
-  $toggleButton.on('click', function() {
-    $(this).toggleClass('button-open');
-    $menuWrap.toggleClass('menu-show');
+    $toggleButton.on('click', function() {
+      $(this).toggleClass('button-open');
+      $menuWrap.toggleClass('menu-show');
+    });
+
+    // Sidebar navigation arrows
+
+    $sidebarArrow.click(function() {
+      $(this).next().slideToggle(300);
+    });
+
   });
 
-  // Sidebar navigation arrows
+//Back to Top //
+$(document).ready(function(){
+  // browser window scroll (in pixels) after which the "back to top" link is shown
+  var offset = 500,
+    //duration of the top scrolling animation (in ms)
+    scroll_top_duration = 700,
+    //grab the "back to top" link
+    $back_to_top = $('.cd-top');
 
-  $sidebarArrow.click(function() {
-    $(this).next().slideToggle(300);
+  //hide or show the "back to top" link
+  $(window).scroll(function(){
+    ( $(this).scrollTop() > offset ) ? $back_to_top.addClass('cd-is-visible') : $back_to_top.removeClass('cd-is-visible cd-fade-out');
+  });
+
+  //smooth scroll to top
+  $back_to_top.on('click', function(event){
+    event.preventDefault();
+    $('body,html').animate({
+      scrollTop: 0 ,
+      }, scroll_top_duration
+    );
   });
 
 });
-
 
 // President page //
 $(document).ready(function(){
