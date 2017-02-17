2007d64eb2baa518e67eb5dddf768e85ebf8c58f
EmeraldLiu <1309810@c126-24.ad.ccsd.net>
2017-02-17 11:54:59 -0800

    dropdown fixed

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,17 +6,19 @@
           $menuWrap = $('.menu-wrap'),
           $sidebarArrow = $('.sidebar-menu-arrow');
 
+      var navLink = $sidebarArrow.parent();
+
+      // Dropdown button
+      $(navLink).click(function() {
+          
+          $(this).children().next().next().slideToggle(300);
+      });
+
       // Hamburger button
 
       $toggleButton.on('click', function() {
         $(this).toggleClass('button-open');
         $menuWrap.toggleClass('menu-show');
-      });
-
-      // Sidebar navigation arrows
-
-      $sidebarArrow.click(function() {
-        $(this).next().slideToggle(300);
       });
 
     //-----------END OF NAVIGATION------------//
